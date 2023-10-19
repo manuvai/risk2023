cdc8a28069ef113a980492111fbbe474945afe73
manuvai <rm90266@gmail.com>
2023-10-19 22:14:59 +0200

    feat: Ajout de tests

---
+++ tests/MainTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(1, 1);
+    }
+}
