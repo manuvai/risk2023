b0c7873c5594c30e35ab9604e422985e121e0531
PHantasm111 <94317933+PHantasm111@users.noreply.github.com>
2023-10-22 19:13:36 +0200

    feature/ajout-classe-regiment
    22/10 PH

---
+++ res/model/Regiment.java
@@ -0,0 +1,15 @@
+package res.model;
+
+public class Regiment {
+    private int nombreRegiment;
+
+    private String typePion;
+
+    public void setNbRegiments(int nombreRegiment) {
+        // TODO: 22/10/2023
+    }
+
+    public void deplacerRegiments(int nombreRegiment, String territoire) {
+        // TODO: 22/10/2023  
+    }
+}
