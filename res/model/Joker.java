5b6c4e5cc28e319665e7624aa5a88af07266e6c1
Yifan0105 <yifanshi0105@gmail.com>
2023-10-23 23:02:02 +0200

    changer

--- res/model/Joker.java
+++ res/model/Joker.java
@@ -1,5 +1,9 @@
 package res.model;
 
-public class Joker {
+public class Joker extends CarteRisk {
+    public Joker(TypePion typePion, Territoire territoire) {
 
+        super(typePion, territoire);
+
+    }
 }
