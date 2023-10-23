b134a2b76c6d0c23b267df707eb64663e307e976
Fatima Mouslim <118826246+FatimaMOUSLIM@users.noreply.github.com>
2023-10-23 22:51:42 +0200

    Update Joueur.java

--- res/model/Joueur.java
+++ res/model/Joueur.java
@@ -59,18 +59,7 @@ public class Joueur {
 
 
 
-public void afficherInformations() {
-        System.out.println("Nom du joueur : " + nom);
-        System.out.println("Prénom du joueur : " + prenom);
-        System.out.println("Armée : " + armee.getNom()); 
-        System.out.println("Cartes du joueur :");
 
-        for (Map.Entry<Carte, Integer> entry : cartes.entrySet()) {
-            Carte carte = entry.getKey();
-            int quantite = entry.getValue();
-            System.out.println(" - " + quantite + "x " + carte.getNom()); 
-        }
-    }
 
  }
 
