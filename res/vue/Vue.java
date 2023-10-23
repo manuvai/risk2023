b7d819799b7bedadbf1de510e5b24a044d40d4e1
Fatima Mouslim <118826246+FatimaMOUSLIM@users.noreply.github.com>
2023-10-23 22:50:44 +0200

    Update Vue.java

--- res/vue/Vue.java
+++ res/vue/Vue.java
@@ -1,4 +1,20 @@
 package res.vue;
 
 public class Vue {
+
+  public void afficherInformations() {
+        System.out.println("Nom du joueur : " + nom);
+        System.out.println("Prénom du joueur : " + prenom);
+        System.out.println("Armée : " + armee.getNom()); 
+        System.out.println("Cartes du joueur :");
+
+        for (Map.Entry<Carte, Integer> entry : cartes.entrySet()) {
+            Carte carte = entry.getKey();
+            int quantite = entry.getValue();
+            System.out.println(" - " + quantite + "x " + carte.getNom()); 
+        }
+    }
+
+ }
+
 }
