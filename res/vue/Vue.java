14067c0faa611e4430ab6098f0b02c06d7e44e61
Fatima Mouslim <118826246+FatimaMOUSLIM@users.noreply.github.com>
2023-10-23 22:39:23 +0200

    Update Vue.java

--- res/vue/Vue.java
+++ res/vue/Vue.java
@@ -1,4 +1,19 @@
 package res.vue;
 
 public class Vue {
+
+
+       public void afficherInformations() {
+        System.out.println("Nom du territoire : " + nom);
+        System.out.println("Propriétaire : " + (proprio != null ? proprio.getNom() : "Aucun"));
+        System.out.println("Nombre d'unités : " + unitésDéployées);
+        System.out.print("Territoires voisins : ");
+        for (Territoire voisin : voisins) {
+            System.out.print(voisin.getNom() + " ");
+        }
+        System.out.println();
+    }
+
+
+
 }
