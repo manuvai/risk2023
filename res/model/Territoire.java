8e0419702103aa3a981b02f77e0b9233569f8a42
Fatima Mouslim <118826246+FatimaMOUSLIM@users.noreply.github.com>
2023-10-23 22:43:36 +0200

    Update Territoire.java

--- res/model/Territoire.java
+++ res/model/Territoire.java
@@ -4,11 +4,7 @@ import java.util.List;
 
 public class Territoire {
     private String nom;
-<<<<<<< HEAD
-   private List<Déploiement> unitésDéployées = new ArrayList<>(); // Nombre d'unités déployées sur le territoire
-=======
-   // private int unitésDéployées; // Nombre d'unités déployées sur le territoire
->>>>>>> 6cf05247cc03dc88f73a3c88523806cabdb05fa8
+    private List<Déploiement> unitésDéployées = new ArrayList<>(); // Nombre d'unités déployées sur le territoire
     private List<Territoire> voisins = new ArrayList<>();
     private Joueur proprio; // Joueur propriétaire du territoire
     
@@ -74,77 +70,6 @@ public void retirerRegiment(Pion pionsToRemove, int qtyToRemove) {
     }
 }
 
-
-     public void afficherInformations() {
-        System.out.println("Nom du territoire : " + nom);
-        System.out.println("Propriétaire : " + (proprio != null ? proprio.getNom() : "Aucun"));
-        System.out.println("Nombre d'unités : " + unitésDéployées);
-        System.out.print("Territoires voisins : ");
-        for (Territoire voisin : voisins) {
-            System.out.print(voisin.getNom() + " ");
-        }
-        System.out.println();
-    }
-
-    /*
-    public void ajouterRegiment(List<Pion> pions, int nombreUnites) {
-        if (nombreUnites <= 0) {
-            // Vérifiez que le nombre d'unités à ajouter est valide.
-            System.out.println("Le nombre d'unités à ajouter doit être supérieur à zéro.");
-            return;
-        }
-        
-        if (pions.size() >= nombreUnites) {
-            // S'il y a suffisamment de pions, ajoutez-les au territoire.
-            for (int i = 0; i < nombreUnites; i++) {
-                Pion pion = pions.remove(0);
-                // Ici, vous pouvez effectuer des opérations pour ajouter le pion au territoire,
-                // par exemple, en ajoutant le pion à une liste d'unités sur le territoire.
-            }
-            System.out.println("Régiment ajouté avec succès !");
-        } else {
-            // Gérez le cas où il n'y a pas suffisamment de pions.
-            System.out.println("Pas assez de pions pour former un régiment.");
-        }
-    }
-
-
-    
-    public void retirerRegiment(List<Pion> pions, int nombreUnites) {
-        if (nombreUnites <= 0) {
-            // Vérifiez que le nombre d'unités à retirer est valide.
-            System.out.println("Le nombre d'unités à retirer doit être supérieur à zéro.");
-            return;
-        }
-        
-        if (nombreUnites <= nombreUnites) {
-            // Vérifiez que le nombre d'unités à retirer est inférieur ou égal au nombre d'unités sur le territoire.
-            if (nombreUnites > this.nombreUnites) {
-                System.out.println("Le nombre d'unités à retirer est supérieur au nombre d'unités sur le territoire.");
-                return;
-            }
-
-            for (int i = 0; i < nombreUnites; i++) {
-                String listeUnites;
-				// Obtenez le pion du territoire, par exemple, en supprimant le premier pion de la liste d'unités sur le territoire.
-                if (!listeUnites.isEmpty()) {
-                    Pion pion = listeUnites.remove(0);
-                    // Effectuez des opérations pour retirer le pion du territoire, par exemple, en le stockant dans une liste de pions disponibles.
-                    pions.add(pion);
-                } else {
-                    // Gérez le cas où il n'y a plus de pions à retirer
-                    System.out.println("Il n'y a plus de pions à retirer.");
-                    break;
-                }
-            }
-            System.out.println("Régiment retiré avec succès !");
-        } else {
-            // Gérez le cas où le nombre d'unités à retirer n'est pas valide
-            System.out.println("Le nombre d'unités à retirer n'est pas valide.");
-        }
-    }
-
-*/
 
     
 }
