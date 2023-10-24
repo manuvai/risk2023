5e84c194ff60997df4d5e3a6db8a2aa9a00024ab
Fatima Mouslim <118826246+FatimaMOUSLIM@users.noreply.github.com>
2023-10-24 22:12:25 +0200

    ajout java doc plateau&continent

--- res/model/Plateau.java
+++ res/model/Plateau.java
@@ -4,10 +4,21 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
 
+    /**
+     * Cette classe représente le plateau de jeu pour un jeu de Risk.
+     */
+
+
 public class Plateau {
     private ArrayList<Continent> continents;
     private ArrayList<Joueur> joueurs;
     private Map<CarteRisk, Integer> cartesPille;
+
+     /**
+     * Distribue les territoires aux joueurs de manière équilibrée.
+     *
+     * @param js La liste des joueurs à qui distribuer les territoires.
+     */
 
     // pluetôt distribuerTerritoires
     public void distribuerCartes(ArrayList<Joueur> js) {
@@ -37,13 +48,30 @@ public class Plateau {
 
     }
 
+
+    /**
+     * Obtient la liste des continents sur le plateau.
+     *
+     * @return La liste des continents sur le plateau.
+     */
+
     public ArrayList<Continent> getContinents() {
         return continents;
     }
 
+     /**
+     * Obtient la carte du jeu associée à chaque quantité de cartes piochées.
+     *
+     * @return Une carte associée à chaque quantité de cartes piochées.
+     */
+
     public Map<CarteRisk, Integer> getCartesPille() {
         return cartesPille;
     }
+
+      /**
+     * Initialise la partie en distribuant les territoires aux joueurs.
+     */
 
     public void initialiserParties() {
 
@@ -51,14 +79,32 @@ public class Plateau {
         // TODO: 23/10/2023
     }
 
+     /**
+     * Obtient la liste des joueurs participants à la partie.
+     *
+     * @return La liste des joueurs participants à la partie.
+     */
+
     public ArrayList<Joueur> getJoueurs() {
         return this.joueurs;
     }
+
+     /**
+     * Obtient les cartes piochées pendant la partie.
+     *
+     * @return Les cartes piochées pendant la partie.
+     */
 
     public Map<CarteRisk, Integer> getCartes() {
         return this.cartesPille;
     }
 
+    /**
+     * Enlève un joueur de la partie.
+     *
+     * @param j Le joueur à enlever de la partie.
+     */
+    
     public void enleverJoueur(Joueur j) {
         this.joueurs.remove(j);
     }
