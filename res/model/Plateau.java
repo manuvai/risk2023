a2bdfa9fa02829de5f380b44863e0ca3d8b2df0e
PHantasm111 <94317933+PHantasm111@users.noreply.github.com>
2023-10-24 12:44:32 +0200

    Update Plateau.java

--- res/model/Plateau.java
+++ res/model/Plateau.java
@@ -13,19 +13,6 @@ public class Plateau {
 
     // pluetôt distribuerTerritoires
     public void distribuerCartes(ArrayList<Joueur> js){
-        /*
-        * Règle du jeux:
-        * Le Général prend la pile des cartes « Risk » et en retire les jokers.
-        * Puis il mélange les cartes et les distribue toutes, une à une dans le sens des aiguilles d’une montre,
-        * en commençant par le joueur situé à sa gauche (si vous jouez à 4 ou à 5, deux joueurs recevront une carte supplémentaire).
-        * Ces cartes indiquent quels sont les territoires que chaque joueur doit occuper en début de partie, sans tenir compte, pour l’instant, des différents types de régiment.
-        * Les joueurs placent un régiment sur chacun des territoires qu’ils ont reçus.
-        * Puis, lorsque tous les territoires sont occupés, chaque joueur, à commencer par celui qui est à gauche du Général,
-        * pose un deuxième régiment sur l’un de ses territoires au choix
-        * (il n’y a pas de limite au nombre de régiments qui peuvent occuper le même territoire, mais chaque territoire doit avoir au moins un régiment)…
-        * Les joueurs suivants procèdent de la même façon et ainsi de suite jusqu’à ce que l’ensemble des régiments de toutes les armées soit sur le plateau.
-        *
-        * */
 
         // Fonctionne:
         // Créez d'abord une liste de territoires et,
@@ -49,9 +36,6 @@ public class Plateau {
                 listeTtContinent.remove(randomIndex);
             }
         }
-
-        // 3. Placer les pions ???
-
 
     }
 
