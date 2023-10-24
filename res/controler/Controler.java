f3378c7683bc08889c6a7f2cecf0f6f9f7b16135
Fatima Mouslim <118826246+FatimaMOUSLIM@users.noreply.github.com>
2023-10-24 18:00:47 +0200

    update controler

--- res/controler/Controler.java
+++ res/controler/Controler.java
@@ -7,11 +7,12 @@ public class Controler {
 
     private Scanner scanner;
 
-    public Controller() {
+    public Controler() {
         this.scanner = new Scanner(System.in);
 
     }
-
+    
+    
     public void phaseAttaque(Joueur joueur) {
         System.out.println("Phase d'attaque pour le joueur : " + joueur.getNom());
 
@@ -25,29 +26,55 @@ public class Controler {
                 break;
             }
 
-            // Demander au joueur de choisir le territoire cible
-            System.out.print("Saisissez le nom du territoire cible : ");
-            String territoireCible = scanner.nextLine();
+            // Déterminer les territoires accessibles à partir du territoire source.
+            List<Territoire> territoiresAccessibles = getTerritoiresAccessiblesDepuis(territoireSource, joueur);
 
-            // Demander au joueur de choisir le nombre de régiments pour l'attaque
-            System.out.print("Saisissez le nombre de régiments à utiliser pour l'attaque : ");
-            int nombreRegiments = scanner.nextInt();
-            scanner.nextLine(); // Nettoyez la nouvelle ligne après la saisie d'un nombre.
+            if (territoiresAccessibles.isEmpty()) {
+                System.out.println("Aucun territoire accessible depuis " + territoireSource + ".");
+            } else {
+                System.out.println("Territoires pouvant être attaqués depuis " + territoireSource + " :");
+                for (Territoire territoire : territoiresAccessibles) {
+                    System.out.println("- " + territoire.getNom());
+                }
 
-            // Traitez les saisies et effectuez l'attaque en conséquence.
-            // Ici, vous pouvez appeler des méthodes pour gérer l'attaque avec les territoires source et cible, et le nombre de régiments.
+                // Demander au joueur de choisir le territoire cible
+                System.out.print("Saisissez le nom du territoire cible : ");
+                String territoireCible = scanner.nextLine();
 
-            System.out.println("Attaque effectuée depuis " + territoireSource + " vers " + territoireCible + " avec " + nombreRegiments + " régiments.");
+                // Demander au joueur de choisir le nombre de régiments pour l'attaque
+                System.out.print("Saisissez le nombre de régiments à utiliser pour l'attaque : ");
+                int nombreRegiments = scanner.nextInt();
+                scanner.nextLine(); // Nettoyez la nouvelle ligne après la saisie d'un nombre.
+
+                // Traitez les saisies et effectuez l'attaque en conséquence.
+                // Ici, vous pouvez appeler des méthodes pour gérer l'attaque avec les territoires source et cible, et le nombre de régiments.
+
+                System.out.println("Attaque effectuée depuis " + territoireSource + " vers " + territoireCible + " avec " + nombreRegiments + " régiments.");
+            }
         }
 
         System.out.println("Fin de la phase d'attaque.");
     }
 
-    public void fermerScanner() {
-        scanner.close();
+    private List<Territoire> getTerritoiresAccessiblesDepuis(String territoireSource, Joueur joueur) {
+        List<Territoire> territoiresAccessibles = new ArrayList<>();
+
+        // Supposons que nous avons une liste de tous les territoires du jeu
+        List<Territoire> tousTerritoires = joueur.getTerritoires(); 
+
+        // Parcourir tous les territoires et les ajouter à la liste des territoires accessibles
+        for (Territoire territoire : tousTerritoires) {
+            if (!territoire.getNom().equals(territoireSource)) {
+                territoiresAccessibles.add(territoire);
+            }
+        }
+
+        return territoiresAccessibles;
     }
 
 
-
+ public void fermerScanner() {
+        scanner.close();
+    }
 
 }
