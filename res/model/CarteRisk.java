881a9e1123dabfe1c61531863aea4996954d2597
Fatima Mouslim <118826246+FatimaMOUSLIM@users.noreply.github.com>
2023-10-24 22:35:20 +0200

    ajout javadoc carteRisk&joker

--- res/model/CarteRisk.java
+++ res/model/CarteRisk.java
@@ -1,9 +1,20 @@
 package res.model;
 
+/**
+ * Représente une carte dans le jeu Risk, associant un type de pion à un territoire.
+ */
+
 public class CarteRisk {
     private TypePion typePion;  
     private Territoire territoire;  
 
+
+    /**
+     * Crée une instance de carte Risk avec un type de pion et un territoire spécifiés.
+     *
+     * @param typePion   Le type de pion associé à la carte.
+     * @param territoire Le territoire associé à la carte.
+     */
 
     public CarteRisk(TypePion typePion, Territoire territoire) {
         this.typePion = typePion;
@@ -11,11 +22,22 @@ public class CarteRisk {
     }
 
 
+    /**
+     * Récupère le type de pion associé à la carte.
+     *
+     * @return Le type de pion de la carte.
+     */
+
     public TypePion getTypePion() {
         return typePion;
     }
 
-
+    /**
+     * Récupère le territoire associé à la carte.
+     *
+     * @return Le territoire de la carte.
+     */
+    
     public Territoire getTerritoire() {
         return territoire;
     }
