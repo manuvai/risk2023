76db886cc0006ac6a022ee00d58898f072673164
PHantasm111 <94317933+PHantasm111@users.noreply.github.com>
2023-10-24 19:18:23 +0200

    finir phaseFortification

--- res/controler/Controler.java
+++ res/controler/Controler.java
@@ -1,56 +1,90 @@
 package res.controler;
 
-import res.model.Déploiement;
-import res.model.Pion;
 import res.model.Territoire;
-import res.model.TypePion;
 
 import java.util.Scanner;
 
 public class Controler {
 
     // Phase Fortification
-    public void phaseFortification() {
-        Scanner sc = new Scanner(System.in);
+    public void phaseFortification() throws Exception {
+        // 1.Demander joueur -> Fortification ?
         while (true) {
-            try {
-                System.out.println("Est-ce que vous voulez fortifier(1/2) ? (Reponse : 1 - Oui, 2 - Non");
-                int resJ = sc.nextInt();
-
-                if (resJ != 1 && resJ != 2) {
-                    throw new Exception("Saissiez numero 1 ou 2 SVP !");
-                } else if (resJ == 1) {
-                    System.out.println("Veuillez sélectionner un territoireSource :");
-                    String territoireSource = sc.nextLine();
-                    // Verify if territoireSource owns by player
-                    /*
-                     * if (this.controlerContinent(territoireSource)){
-                     *   continue;
-                     * } else {
-                     *   retourne au debut
-                     *
-                     */
-
-                    System.out.println("Veuillez sélectionner un territoireCible :");
-                    String territoireCible = sc.nextLine();
-                    // Verify if territoireCible owns by player
-
-                    System.out.println("Veuillez sélectionner le nombre de régiment :");
-                    // Afficher le nombre de rigément du territoire territoireSource
-
-                    int nbRegiment = sc.nextInt();
-                    // Verify if player have enough nb regiment
-                    // and at least one rigement stay at territoireSource
-
-                    // start to move ->
-                    deplacerRegiment(territoireSource, territoireCible, nbRegiment);
-                    break;
-
-                } else {
+            int resJ = demanderFortification();
+            if (resJ != 1 && resJ != 2) {
+                throw new Exception("Saissiez numero 1 ou 2 SVP !");
+            } else {
+                // 2.Commencer Fortification
+                if (resJ == 1) {
+                    Territoire tS = DemanderTerritoireSource();
+                    Territoire tC = DemanderTerritoireCible();
+                    int nbRegiment = DemanderNbRegimentDeplace();
+                    deplacerRegiment(tS, tC, nbRegiment);
                     break;
                 }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+    public int demanderFortification() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        // Demander si joueur va commencer l'étape de fortification
+        System.out.println("Est-ce que vous voulez fortifier(1/2) ? (Reponse : 1 - Oui, 2 - Non");
+        int resJ = sc.nextInt();
+        sc.close();
+        return resJ;
+
+    }
+
+    public Territoire DemanderTerritoireSource() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Veuillez sélectionner un territoireSource :");
+            String territoireSource = sc.nextLine();
+            // Si joueur controle ce territoire, on changer le String -> Territoire
+            if (joueur.controleTerritoire(territoireSource)) { // Boolean <- controleTerritoire(territoireSource)
+                Territoire tS = joueur.ExchangeStringTerritoire(territoireSource); // Territoire <- ExchangeStringTerritoire(territoireSource)
+                sc.close();
+                return tS;
+            } else {
+                System.err.println("Vous controle pas ce territoire ! Ressayer !");
+            }
+        }
+    }
+
+    public Territoire DemanderTerritoireCible() {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Veuillez sélectionner un territoireCible :");
+            String territoireCible = sc.nextLine();
+
+            // Si joueur controle ce territoire, on changer le String -> Territoire
+            if (joueur.controleTerritoire(territoireSource)) { // Boolean <- controleTerritoire(territoireSource)
+                Territoire tC = joueur.ExchangeStringTerritoire(territoireCible); // Territoire <- ExchangeStringTerritoire(territoireSource)
+                sc.close();
+                return tC;
+            } else {
+                System.err.println("Vous controle pas ce territoire ! Ressayer !");
+            }
+        }
+    }
+
+    public int DemanderNbRegimentDeplace(Territoire tS) {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Veuillez sélectionner un territoireCible :");
+            System.out.println("Vous avez" + tS.getNbRegiment() + " regiments ici !");// int <- getNbRegiment()
+            System.out.println("Vous pouvez deplacer 0 -> " + (tS.getNbRegiment() - 1) + "regiments");
+            int nbRegiment = sc.nextInt();
+
+            if (nbRegiment > (tS.getNbRegiment() - 1)) {
+                System.err.println("Vous n'avez pas assez de regiments");
+            } else if (nbRegiment < 0) {
+                System.err.println("nbRegiment ne peut pas < 0");
+            } else {
+                sc.close();
+                return nbRegiment;
             }
         }
     }
@@ -59,40 +93,8 @@ public class Controler {
             Territoire territoireSource,
             Territoire territoireCible,
             int nombreRegiment) {
-
-        // Calculer nb de chaque pion
-        int nb_ARTILLERIE = nombreRegiment / 10;
-        int nb_INFANTERIE = (nombreRegiment - nombreRegiment / 10 * 10) / 5;
-        int nb_CAVALERIE = nombreRegiment - (nombreRegiment / 10 * 10) - (nombreRegiment / 5 * 5);
-        // Ajouter dans le territoireCible
-        // Pion Cavalerie
-        Pion pC = new Pion("pionCavalerie", TypePion.CAVALERIE);
-
-        Déploiement dC = new Déploiement(pC);
-        dC.ajouterQuantites(nb_CAVALERIE);
-
-        territoireCible.ajouterRegiment(pC, nb_CAVALERIE);
-
-        // Pion Infanterie
-        Pion pI = new Pion("pionInfanterie", TypePion.INFANTERIE);
-
-        Déploiement dI = new Déploiement(pI);
-        dI.ajouterQuantites(nb_INFANTERIE);
-
-        territoireCible.ajouterRegiment(pI, nb_INFANTERIE);
-
-        // Pion Artillerie
-        Pion pA = new Pion("pionArtillerie", TypePion.ARTILLERIE);
-
-        Déploiement dA = new Déploiement(pA);
-        dA.ajouterQuantites(nb_INFANTERIE);
-
-        territoireCible.ajouterRegiment(pA, nb_ARTILLERIE);
-
-        // Enlever le nb de Regiment de territoireSource
-        territoireSource.retirerRegiment(pC, nb_CAVALERIE);
-        territoireSource.retirerRegiment(pI, nb_INFANTERIE);
-        territoireSource.retirerRegiment(pA, nb_ARTILLERIE);
+        joueur.deplacerRegiment(territoireSource, territoireCible, nombreRegiment);
     }
+
 }
 
